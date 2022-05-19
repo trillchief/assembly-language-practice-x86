0ade393802a39ceb8cb2ed85e59b1068e9762f6c
Tyler Jay <92478850+trillchief@users.noreply.github.com>
2022-05-19 06:35:09 -0400

    hello world
    saying hello world in assembly

---
+++ hello_world.asm
@@ -0,0 +1,25 @@
+; hello_world.asm
+;
+; Author: Tyler Mears
+; Date: 05/19/2022
+
+global_start
+
+section .text:
+
+_start:
+    mov eax, 0x4                ; use the write syscall
+    mov ebx, 1                  ; use stdout as the fd
+    mov ecx, message            ; use the message as the buffer
+    mov edx, message_length     ; and supply the length
+    int 0x80                    ; invoke the syscall
+
+    ; now gracefully exit the program
+
+    mov eax, 0x1
+    mov ebx, 0
+    int 0x80
+
+section .data:
+    message: db "Hello World!", 0xA
+    message_length equ $-message
